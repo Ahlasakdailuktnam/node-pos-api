afa51ee0514c8cb22264e6f0fa7b31b16162c4b9
phalla <ahlaluktnam@gmail.com>
2026-05-12 22:11:52 +0700

    add delete feature

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -7,14 +7,30 @@ exports.getCategory = async (req,res) => {
    });
 };
 
-exports.createCategory = (req,res) => {
+exports.createCategory = async (req,res) => {
+  var sql = "INSERT INTO category(Name,Description,Status,ParentId) VALUES (:Name,:Description,:Status,:ParentID) ";
+  var [data] = await db.query(sql , {
+    Name: req.body.Name,
+    Description: req.body.Description,
+    Status: req.body.Status,
+    ParentID: req.body.ParentID,
+  });
+  res.json({
+    data: data,
+  })
+}
+exports.updateCategory = (req,res)=> {
     res.json({
-        list:[2],
+        list:[3]
     })
 }
-
-exports.deleteCategory =(req,res) => {
-    res.json({
-        list : [4]
-    })
+exports.deleteCategory = async (req,res) => {
+    var sql = "DELETE FROM category WHERE Id=:Id";
+  var [data] = await db.query(sql , {
+   Id: req.body.id,
+  });
+  res.json({
+    data: data,
+    message: "delete bn jork jum",
+  })
 }
